c24291b37bc87681669b8c6848102663bbbf1974
Robby Kraft <robbykraft@gmail.com>
2019-03-08 15:19:36 -0500

    adding mouse follow

---
+++ 01_mouseFollow/src/ofApp.cpp
@@ -0,0 +1,76 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+	ofBackground(0);
+}
+
+//--------------------------------------------------------------
+void ofApp::update(){
+	// easing the position toward the mouse
+//	position.x = mouseX * 0.01 + position.x * 0.99;
+//	position.y = mouseY * 0.01 + position.y * 0.99;
+	
+	position.interpolate(ofPoint(mouseX, mouseY), 0.01);
+
+}
+
+//--------------------------------------------------------------
+void ofApp::draw(){
+	ofDrawCircle(position, 30);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y ){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseEntered(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseExited(int x, int y){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg){
+
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo){ 
+
+}
